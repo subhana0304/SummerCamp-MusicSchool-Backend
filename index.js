f6e3cfe2d9fcc6e57c51b940665b1218579bbfaa
Subhana <subhana5721194@gmail.com>
2023-06-09 12:01:47 +0600

    cart delete added

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zmsfvx2.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -59,6 +59,13 @@ async function run() {
         console.log(item);
         const result = await cartsCollection.insertOne(item);
         res.send(result);
+    })
+
+    app.delete('/carts/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await cartsCollection.deleteOne(query);
+      res.send(result);
     })
 
 
