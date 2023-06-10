114c9d00ce30dc602a1d4a1c0f19171c9e2610da
Subhana <subhana5721194@gmail.com>
2023-06-10 11:10:40 +0600

    patch updated

--- index.js
+++ index.js
@@ -52,11 +52,25 @@ async function run() {
       res.send(result);
     })
 
+    app.patch('/users/admin/:id', async(req, res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
+    // classes Api
     app.get('/classes', async(req, res)=>{
         const result = await classesCollection.find().toArray();
         res.send(result);
     })
 
+    // instructors api
     app.get('/instructors', async(req, res)=>{
         const result = await instructorsCollection.find().toArray();
         res.send(result);
